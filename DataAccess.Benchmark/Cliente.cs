d4386c85d6e82491fa212e03251fd67f302c6ba1
Bruno <bruno.freschi2@gmail.com>
2026-03-16 11:01:27 -0300

    add prop

---
+++ DataAccess.Benchmark/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Benchmark;
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public bool Ativo { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+}
