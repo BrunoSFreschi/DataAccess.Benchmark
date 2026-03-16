cf968fbd0e90532449ed9d10e5b1e50468e72055
Bruno <bruno.freschi2@gmail.com>
2026-03-16 11:01:47 -0300

    add class ClienteBenchmark

---
+++ DataAccess.Benchmark/ClienteBenchmark.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace DataAccess.Benchmark;
+
+[MemoryDiagnoser]
+public class ClienteBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+        DbFactory.ResetDatabase();
+    }
+}
