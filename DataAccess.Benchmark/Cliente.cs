a879a598d90a61f9c40858c7b05ae71e6f330e7f
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-03-19 23:05:04 -0300

    update

---
+++ DataAccess.Benchmark/Cliente.cs
@@ -0,0 +1,8 @@
+﻿public class Cliente
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Mail { get; set; }
+    public DateTime CreateAt { get; set; }
+    public bool Activated { get; set; }
+}
