f0e0d74de32c632b1f30efdb537fbd9c63a07d19
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-03-25 22:55:56 -0300

    update class

--- DataAccess.Benchmark/DbFactory.cs
+++ DataAccess.Benchmark/DbFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
 
 namespace DataAccess.Benchmark;
 
@@ -7,7 +8,7 @@ public class DbFactory
     public const string ConnectionString = "Data Source=benchmark.db";
 
     public static SqliteConnection Create()
-        => new SqliteConnection(ConnectionString);
+        => new(ConnectionString);
 
     public static void ResetDatabase()
     {
@@ -26,8 +27,24 @@ public class DbFactory
                 Email TEXT NOT NULL,
                 Ativo INTEGER NOT NULL,
                 DataCriacao TEXT NOT NULL
-            );
-        ";
+            );";
         command.ExecuteNonQuery();
+    }
+
+    // Inicializa banco com WAL ativado
+    internal static void InicializarBanco()
+    {
+        using var conn = new SQLiteConnection(ConnectionString);
+        conn.Open();
+
+        using var cmd = conn.CreateCommand();
+
+        cmd.CommandText = "PRAGMA journal_mode=WAL;";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "PRAGMA synchronous=NORMAL;";
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine("- Banco inicializado com WAL e otimizações");
     }
 }
