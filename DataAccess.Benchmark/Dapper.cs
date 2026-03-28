2cda7b349039fea3fe34b82a782b57b3aa04f9f9
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-03-28 08:39:34 -0300

    refac

--- DataAccess.Benchmark/Dapper.cs
+++ DataAccess.Benchmark/Dapper.cs
@@ -1,17 +1,17 @@
 ﻿using System.Data;
 using System.Data.SQLite;
 using System.Diagnostics;
+using Dapper;
 
 namespace DataAccess.Benchmark;
 
 internal class Dapper
 {
+
     private const string ConnectionString = "Data Source=benchmark.db;";
     private const int Total = 1_000_000;
-    private const int BatchSize = 500;
 
-
-    internal static void InsertSimples(int total = Total)
+    internal static void InsertSimplesDapper(int total = Total)
     {
         var sw = Stopwatch.StartNew();
 
@@ -19,25 +19,20 @@ internal class Dapper
         conn.Open();
 
         using var transaction = conn.BeginTransaction();
-        using var cmd = conn.CreateCommand();
 
-        cmd.CommandText = @"
-            INSERT INTO Pessoas (Nome, Email, Ativo, DataCriacao)
-            VALUES (@Nome, @Email, @Ativo, @DataCriacao)";
-
-        var pNome = cmd.Parameters.Add("@Nome", DbType.String);
-        var pEmail = cmd.Parameters.Add("@Email", DbType.String);
-        var pAtivo = cmd.Parameters.Add("@Ativo", DbType.Int32);
-        var pData = cmd.Parameters.Add("@DataCriacao", DbType.String);
+        var sql = @"
+        INSERT INTO Pessoas (Nome, Email, Ativo, DataCriacao)
+        VALUES (@Nome, @Email, @Ativo, @DataCriacao)";
 
         for (int i = 1; i <= total; i++)
         {
-            pNome.Value = $"Nome {i}";
-            pEmail.Value = $"email{i}@teste.com";
-            pAtivo.Value = i % 2;
-            pData.Value = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
-
-            cmd.ExecuteNonQuery();
+            conn.Execute(sql, new
+            {
+                Nome = $"Nome {i}",
+                Email = $"email{i}@teste.com",
+                Ativo = i % 2,
+                DataCriacao = DateTime.UtcNow
+            }, transaction);
 
             if (i % 10_000 == 0)
                 Console.Write($"\rProgresso: {i:N0}/{total:N0}");
@@ -46,7 +41,7 @@ internal class Dapper
         transaction.Commit();
         sw.Stop();
 
-        PrintResultado("Insert Simples", total, sw.Elapsed);
+        PrintResultado("Insert Simples Dapper", total, sw.Elapsed);
     }
 
     private static void PrintResultado(string descricao, int total, TimeSpan tempo)
