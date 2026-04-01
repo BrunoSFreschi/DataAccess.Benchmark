fea291574da06484722a311384ba5d205f021c94
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-03-31 23:15:30 -0300

    create

---
+++ DataAccess.Benchmark/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccess.Benchmark;
+
+internal class Entity
+{
+    private const string ConnectionString = "Data Source=benchmark.db;";
+    private const int Total = 1_000_000;
+
+    internal static void InsertBatch(int total = Total)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void InsertParalelo(int total = Total)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void InsertSimples(int total = Total)
+    {
+        throw new NotImplementedException();
+    }
+}
