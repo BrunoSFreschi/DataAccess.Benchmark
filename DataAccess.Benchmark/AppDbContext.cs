1fcaef17e5383593ee130d60501192d1ece0e14c
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-04-03 15:07:32 -0300

    create

---
+++ DataAccess.Benchmark/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Benchmark;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+                .UseSqlite("Data Source=benchmark.db;")
+                .EnableSensitiveDataLogging(false)
+                .EnableDetailedErrors(false);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var entity = modelBuilder.Entity<Cliente>();
+
+        // 🔗 Nome da tabela (igual ao seu benchmark)
+        entity.ToTable("Pessoas");
+
+        entity.HasKey(c => c.Id);
+
+        entity.Property(c => c.Id)
+            .ValueGeneratedOnAdd();
+
+        // 🔥 Mapeando nomes corretos das colunas
+        entity.Property(c => c.Name)
+            .HasColumnName("Nome")
+            .IsRequired()
+            .HasMaxLength(200);
+
+        entity.Property(c => c.Mail)
+            .HasColumnName("Email")
+            .IsRequired()
+            .HasMaxLength(200);
+
+        entity.Property(c => c.Activated)
+            .HasColumnName("Ativo")
+            .IsRequired();
+
+        entity.Property(c => c.CreateAt)
+            .HasColumnName("DataCriacao")
+            .IsRequired()
+            .HasColumnType("TEXT"); // SQLite padrão
+    }
+}
